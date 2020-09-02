026e5b9264dd6d531c1f1a0f7f481b03e68faae3
Zachary Reiss <zachary.reiss123@gmail.com>
2020-09-01 21:35:25 -0600

    Add constants file - Should have working example once weird bug with comparing chars is fixed

--- Program.cs
+++ Program.cs
@@ -8,64 +8,113 @@ namespace BankOCR
     {
         static void Main(string[] args)
         {
-            /*
-            for each 3x3 array take each cell(number)
-            put in single array
-            convert array into string
-            compare string to static values
-            concat value into parsed accout number
-            */
-            string input = @" _  _     _  _     _  _
-                            | _| _||_||_ |_   | _| _|
-                            ||_  _|  | _||_|  ||_  _|
-                                                     ";
+            string input = @"    _  _     _  _     _  _
+  | _| _||_||_ |_   | _| _|
+  ||_  _|  | _||_|  ||_  _|
+                           ";             
 
-            // var newString = input.Split(Environment.NewLine);
-            // var splitString = string.Join(",", newString);
-            // Console.WriteLine(newString[0]);            
-            Console.WriteLine(parseAcctNumber(input));
+            buildAccountNumber(ParseAccountNumber(input));
         }
 
-        
-        public static char[] parseAcctNumber(string input){
-            //input is first line of character
-            var source = input.ToCharArray();
+        private static char[][] ParseAccountNumber(string input){
+
             
-            char[] firstNumber = new char[12];            
+            var newString = input.Split("\n")
+            .Select(l => l.Replace("\r", string.Empty))
+            .ToList();
+            
+            char[] lineOne = newString[0].ToCharArray();         
+            char[] lineTwo = newString[1].ToCharArray();         
+            char[] lineThree = newString[2].ToCharArray();
 
+            char[][] lines = new char [3][]
+            {
+                lineOne,
+                lineTwo,
+                lineThree
+            };
+
+            char[][] acctNumberArray = new char [9][];
+
+            char[] firstNumber = new char[12];
             char[] secondNumber = new char[12];
-
             char[] thirdNumber = new char[12];
-
             char[] fourthNumber = new char[12];
-
             char[] fifthNumber = new char[12];
-
             char[] sixthNumber = new char[12];
-
             char[] seventhNumber = new char[12];
-
             char[] eighthNumber = new char[12];
-
             char[] ninthNumber = new char[12];
 
-            for(int i = 0; i < 4; i++){
-                for(int j = 0; j < 27; j+=3){
-
-                    Array.Copy(source, 0, firstNumber, j, 3);
-                    Array.Copy(source, 3, secondNumber, j, 3);
-                    Array.Copy(source, 6, thirdNumber, j, 3);
-                    Array.Copy(source, 9, fourthNumber, j, 3);
-                    Array.Copy(source, 12, fifthNumber, j, 3);
-                    Array.Copy(source, 15, sixthNumber, j, 3);
-                    Array.Copy(source, 18, seventhNumber, j, 3);
-                    Array.Copy(source, 21, eighthNumber, j, 3);
-                    Array.Copy(source, 24, ninthNumber, j, 3);
-
-                }  
-                return firstNumber;             
+            int k = 0;
+            for (int i = 0; i < 3; i++) {                
+                for(int j = 0; j < 3; j++) {
+                        firstNumber[k] = lines[i][j];
+                        secondNumber[k] = lines[i][3 + j];
+                        thirdNumber[k] = lines[i][6 + j];
+                        fourthNumber[k] = lines[i][9 + j];
+                        fifthNumber[k] = lines[i][12 + j];
+                        sixthNumber[k] = lines[i][15 + j];
+                        seventhNumber[k] = lines[i][18 + j];
+                        eighthNumber[k] = lines[i][21 + j];
+                        ninthNumber[k] = lines[i][24 + j];
+                        k++;
+                }
             }
-            return firstNumber;
+
+            acctNumberArray[0] = firstNumber;
+            acctNumberArray[1] = secondNumber;
+            acctNumberArray[2] = thirdNumber;
+            acctNumberArray[3] = fourthNumber;
+            acctNumberArray[4] = fifthNumber;
+            acctNumberArray[5] = sixthNumber;
+            acctNumberArray[6] = seventhNumber;
+            acctNumberArray[7] = eighthNumber;
+            acctNumberArray[8] = ninthNumber;
+
+            return acctNumberArray;
+
+            // Console.WriteLine(firstNumber[5].CompareTo(Constants.ONE[5]));
+            // Console.WriteLine(secondNumber);
+            // Console.WriteLine(Constants.TWO.ToCharArray());
+        }
+
+        public static string buildAccountNumber(char[][] acctNumArray){
+            string acctNum = "";
+            foreach(var number in acctNumArray){                
+                if(number.SequenceEqual(Constants.ZERO)){
+                    acctNum.Concat("0");
+                }
+                else if(number.SequenceEqual(Constants.ONE)){
+                    acctNum.Concat("1");
+                }
+                else if(number.SequenceEqual(Constants.TWO)){
+                    acctNum.Concat("2");
+                }
+                else if(number.SequenceEqual(Constants.THREE)){
+                    acctNum.Concat("3");
+                }
+                else if(number.SequenceEqual(Constants.FOUR)){
+                    acctNum.Concat("4");
+                }
+                else if(number.SequenceEqual(Constants.FIVE)){
+                    acctNum.Concat("5");
+                }
+                else if(number.SequenceEqual(Constants.SIX)){
+                    acctNum.Concat("6");
+                }
+                else if(number.SequenceEqual(Constants.SEVEN)){
+                    acctNum.Concat("7");
+                }
+                else if(number.SequenceEqual(Constants.EIGHT)){
+                    acctNum.Concat("8");
+                }
+                else if(number.SequenceEqual(Constants.NINE)){
+                    acctNum.Concat("9");
+                }                            
+            }
+            return acctNum;
         }
     }
 }
+
---
+++ Shared/Constants.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankOCR
+{
+    class Constants
+    {
+        public static string ZERO = " _ | ||_|   ";
+        public static string ONE = "     |  |   ";
+        public static string TWO = " _  _||_    ";
+        public static string THREE = " _  _| _|   ";
+        public static string FOUR = "   |_|  |   ";
+        public static string FIVE = " _ |_  _|   ";
+        public static string SIX = " _ |_ |_|   ";
+        public static string SEVEN = " _   |  |   ";
+        public static string EIGHT = " _ |_||_|   ";
+        public static string NINE = " _ |_| _|   ";
+    }
+}
